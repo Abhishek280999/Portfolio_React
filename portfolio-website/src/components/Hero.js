5d8bde9d59ef0921143d61c51d9e8e708de01cd1
Abhishek Darwekar <117111036+Abhishek280999@users.noreply.github.com>
2023-06-07 13:37:02 +0530

    Update Hero.js

--- portfolio-website/src/components/Hero.js
+++ portfolio-website/src/components/Hero.js
@@ -2,7 +2,7 @@ import React from "react";
 
 // import woman image
 import WomanImg from "../assets/img/banner-women2.png";
-import resume from "../assets/Abhishek_darwekar.pdf";
+import resume from "../assets/Abhishek.pdf";
 import Socials from "./Socials";
 const Hero = () => {
   return (
