e9b535cc8ec4f99571fe3aae678418e0ec7fba06
Abhishek280999 <darwekar155@gmail.com>
2023-08-31 21:11:17 +0530

    k

--- portfolio-website/src/components/Hero.js
+++ portfolio-website/src/components/Hero.js
@@ -10,6 +10,7 @@ const Hero = () => {
       id="home"
       className="lg:h-[85vh] flex items-center bg-primary lg:bg-cover lg:bg-center lg:bg-no-repeat py-32 lg:py-0 overflow-hidden"
     >
+    
       <div className="container mx-auto h-full">
         <div className="flex items-center h-full pt-8">
           <div className="flex-1 flex flex-col items-center lg:items-start">
